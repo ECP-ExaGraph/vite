c9101aac7c9fbdd78121c9bb266afdc44d4beec4
ghos167 <sayan.ghosh@pnnl.gov>
2026-01-23 11:50:36 -0800

    finalized pajek format writing w handling gaps in vertices

--- converters/pajek.cpp
+++ converters/pajek.cpp
@@ -104,7 +104,7 @@ void loadPajekFile(Graph *&g, const std::string &fileName, bool indexOneBased, W
   // currently assume w=1.0
   do {
     std::getline(ifs, line);
-    //line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
+    line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
 
     GraphElem v0, v1;
     GraphWeight w = 1.0;
@@ -162,134 +162,32 @@ void loadPajekFile(Graph *&g, const std::string &fileName, bool indexOneBased, W
     edgeCount[edgeList[f].i_+1]++;
   }
  
+  GraphElem old_numVertices = numVertices;
   numVertices = edgeCount.size()-1;
   numEdges    = edgeList.size(); 
   std::cout << "Legitimate {#Vertices, #Edges} recorded while reading the file: " << numVertices << ", " << numEdges << std::endl;
 
   g = new Graph(numVertices, numEdges);
   processGraphData(*g, edgeCount, edgeList, numVertices, numEdges);
-  
-  t1 = mytimer();  
-  std::cout << "Total graph processing time: " << (t1 - t0) << std::endl;
-} // loadPajekFile
-
-void loadPajekFileOrig(Graph *&g, const std::string &fileName, bool indexOneBased, Weight_t wtype)
-{
-  std::ifstream ifs;
-
-  double t0, t1;
-
-  t0 = mytimer();
-
-  ifs.open(fileName.c_str(), std::ifstream::in);
-  if (!ifs) {
-    std::cerr << "Error opening Pajek format file: " << fileName << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  GraphElem numEdges = 0, numVertices = 0;
-  
-  /// http://mrvar.fdv.uni-lj.si/pajek/DrawEPS.htm
-  // *Arcs are directed whereas *Edges indicate undirected
  
-  std::string line;
-  std::string target_word_vertices = "*Vertices";
-  bool found_edges = false;
-      
-  while(std::getline(ifs, line)) {
-       if (line.find(target_word_vertices) != std::string::npos) 
-          numVertices = std::stol(line.substr(line.find(' ') + 1));
-       if (line.find("*Arcs") != std::string::npos || line.find("*Edges") != std::string::npos) {
-          found_edges = true;
-          break;
-       } 
+  // write the mapping excluding gaps
+  if (old_numVertices > numVertices) {
+	  std::ofstream ofs;
+	  std::string fileNameExt = fileName + ".mapping." + std::to_string(v_id);
+	  ofs.open(fileNameExt.c_str(), std::ofstream::out);
+
+	  if (!ofs) {
+		  std::cerr << "Error creating file to store mappings: " << fileNameExt << std::endl;
+		  exit(EXIT_FAILURE);
+	  }
+
+	  for (auto vm: vmap)
+		  ofs << vm.first << " " << vm.second << std::endl;
+	  ofs.close();
+	  std::cout << "Dissimilar #vertices detected when reading edge list, old-new vertex ID mappings are written in file: " << fileNameExt << std::endl;
   }
 
-  if (numVertices > 0 && found_edges) {
-       std::cout << "Reading edges from Pajek format file: " << fileName << " with #Vertices listed as: " << numVertices << std::endl;
-  }
-  else {
-    std::cerr << "Unable to find *Vertices and edge lists in Pajek format file: " << fileName << std::endl;
-    exit(EXIT_FAILURE);
-  }
  
-  std::vector<short> markVertices(numVertices, 0);
-  std::vector<GraphElemTuple> edgeList;
-  std::vector<GraphElem> edgeCount;
-
-  // TODO FIXME accept weights as third parameter per line
-  // currently assume w=1.0
-  do {
-    
-    std::getline(ifs, line);
-
-    line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
-
-    GraphElem v0, v1;
-    GraphWeight w = 1.0;
-    std::istringstream iss(line);
-
-    iss >> v0 >> v1;
-
-    if (v0 >= std::numeric_limits<GraphElem>::max() || v1 >= std::numeric_limits<GraphElem>::max())
-        continue;
-    if (v0 >= numVertices || v1 >= numVertices) {
-       std::cerr << "Error: vertex indices are larger than the #Vertices: " << v0 << ", " << v1 << std::endl;
-       exit(EXIT_FAILURE);
-    }
-     
-    if (indexOneBased) {
-        v0--; 
-        v1--;
-    }
-    
-    if (wtype == RND_WEIGHT)
-        w = genRandom(RANDOM_MIN_WEIGHT, RANDOM_MAX_WEIGHT);
-    
-    if (wtype == ABS_WEIGHT)
-        w = std::fabs(w);
-
-    edgeList.push_back({v0, v1, w});
-    edgeList.push_back({v1, v0, w});
-    markVertices[v0] = 1;
-    markVertices[v1] = 1;
-  } while (!ifs.eof());
-  
-  ifs.close();
-
-  std::map<GraphElem, GraphElem> vmap;
-  GraphElem v_id = 0;
-  
-  for (GraphElem k = 0; k < numVertices; k++)
-  {
-    if (markVertices[k] == 1)
-    {
-      vmap.insert({k, v_id});
-      v_id++;
-    }
-  }
-
-  edgeCount.resize(v_id+1, 0);
-  
-  // adjust for duplicates 
-  std::sort(edgeList.begin(), edgeList.end());
-  auto last = std::unique(edgeList.begin(), edgeList.end());
-  edgeList.erase(last, edgeList.end());
-  
-  /// adjust edge count/list to address gaps  
-  for (auto el: edgeList) {
-    el.i_  = vmap[el.i_];
-    el.j_  = vmap[el.j_];
-    edgeCount[el.i_+1]++;
-  }
- 
-  numVertices = edgeCount.size()-1;
-  numEdges    = edgeList.size(); 
-  std::cout << "Legitimate {#Vertices, #Edges} recorded while reading the file: " << numVertices << ", " << numEdges << std::endl;
-
-  g = new Graph(numVertices, numEdges);
-  processGraphData(*g, edgeCount, edgeList, numVertices, numEdges);
-  
   t1 = mytimer();  
   std::cout << "Total graph processing time: " << (t1 - t0) << std::endl;
 } // loadPajekFile
--- converters/pajek.hpp
+++ converters/pajek.hpp
@@ -56,6 +56,5 @@
 #include "../graph.hpp"
 
 void loadPajekFile(Graph *&g, const std::string &fileName, bool indexOneBased = true, Weight_t wtype = ONE_WEIGHT);
-void loadPajekFileOrig(Graph *&g, const std::string &fileName, bool indexOneBased = true, Weight_t wtype = ONE_WEIGHT);
 
 #endif // __PAJEK_H
